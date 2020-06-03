853deef40c2143f0c53d191c1500a8112a8814a0
Eric W <ericwanjohi.ew@gmail.com>
2020-06-03 19:20:37 +0300

    - Create transitionToDetailsActivity method in JobsViewHolder to handle transition to details activity. - Refactor TextView items in JobsViewHolder. - Create JobDetails activity, activity_job_details and content_job_details layout. - Create new string values in strings file. - Create new dimensions. - Create new styles. - Add meta data to JobDetails activity tag in the manifest file.

--- app/src/main/java/ke/co/droidsense/jobs/Adapters/JobsViewHolder.java
+++ app/src/main/java/ke/co/droidsense/jobs/Adapters/JobsViewHolder.java
@@ -1,6 +1,7 @@
 package ke.co.droidsense.jobs.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,12 +12,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import es.dmoral.toasty.Toasty;
 import ke.co.droidsense.jobs.Models.Job;
 import ke.co.droidsense.jobs.R;
+import ke.co.droidsense.jobs.UI.JobDetails;
 
 public class JobsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     //Member Variables.
     private View view;
     private Context context;
     private Button bidButton;
+    private TextView jobTitle;
+    private TextView jobDesc;
+    private TextView jobBudget;
 
     //Job ViewHolder
     public JobsViewHolder(@NonNull View itemView) {
@@ -32,15 +37,15 @@ public class JobsViewHolder extends RecyclerView.ViewHolder implements View.OnCl
 
     public void bindJobItems(Job job){
         //Get View items.
-        TextView jobTitle = view.findViewById( R.id.job_title_text );
-        TextView jobDesc = view.findViewById( R.id.job_desc_text );
-        TextView jobBudget = view.findViewById( R.id.job_budget_text );
+        jobTitle = view.findViewById( R.id.job_title_text );
+        jobDesc = view.findViewById( R.id.job_desc_text );
+        jobBudget = view.findViewById( R.id.job_budget_text );
         bidButton = view.findViewById( R.id.new_job_button );
 
         //Set Text on view Items
         jobTitle.setText( job.getJob_title() );
         jobDesc.setText( job.getJob_description() );
-        jobBudget.setText("Budget (Ksh) " + job.getJob_budget() );
+        jobBudget.setText( "Budget " + job.getJob_budget() + "/=" );
         bidButton.setOnClickListener( this );
     }
 
@@ -48,11 +53,23 @@ public class JobsViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     @Override
     public void onClick(View clickedView) {
         //Check view clicked.
-        if (clickedView.getId() == R.id.new_job_button){
-            //Toast item for now
-            Toasty.info( context, "Item clicked..." );
+        if (clickedView.getId() == R.id.job_item) {
+            //Handle transition to job Detail Activity for more information...
+            transitionToDetailsActivity();
         }
-        Toasty.normal( context, "Item View Clicked" );
+    }
 
+    //Transition method...
+    private void transitionToDetailsActivity() {
+
+        //Create New Intent...
+        Intent jobDetailsIntent = new Intent( context.getApplicationContext(), JobDetails.class );
+        context.startActivity( jobDetailsIntent );
+
+        //Toast...
+        Toasty.info( context, " Getting more information on "
+                + jobTitle.getText().toString().trim()
+                + " job.", Toasty.LENGTH_LONG )
+                .show();
     }
 }
